cef64f424cb7ca7db4e04ff1d26dd7988d33e128
azuredragoon <terencenmnpan@gmail.com>
2017-06-07 14:18:45 -0700

    Added call webservice from page
    Added call webservice from page

---
+++ tpTestAppWebservices/ViewController/adfmsrc/com/example/tp/app/tptestwebservices/view/beans/StockExampleBean.java
@@ -0,0 +1,13 @@
+package com.example.tp.app.tptestwebservices.view.beans;
+
+public class StockExampleBean {
+    String defaultStockValue = "SPY";
+
+    public void setDefaultStockValue(String defaultStockValue) {
+        this.defaultStockValue = defaultStockValue;
+    }
+
+    public String getDefaultStockValue() {
+        return defaultStockValue;
+    }
+}
